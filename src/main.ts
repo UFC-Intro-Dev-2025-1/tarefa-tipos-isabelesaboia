026a8e40f4d51eafc905022d976454ae862771d7
isabelesaboia <124289421+isabelesaboia@users.noreply.github.com>
2025-05-23 12:58:23 +0000

    solução da tarefa

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,45 @@
 // Escreva seu código aqui!
+const tipo: string = 'number';
+console.log(tipo);
+
+const texto: string = 'Número de portas';
+console.log(texto);
+
+const numeroDePortas: number = 2;
+console.log(numeroDePortas);
+
+const tipodois: string = 'string';
+console.log(tipodois);
+
+let nomedaprofessora: string = 'Nome da Professora: Lana Mesquita';
+console.log(nomedaprofessora);
+
+let tipotres: string = 'boolean'
+console.log(tipotres);
+
+let textodois: string = 'Quinta feira tem aula de programação?'
+console.log(textodois);
+
+let quintaTemAulaDeProgramacao: boolean = true; 
+console.log(quintaTemAulaDeProgramacao);
+
+let tipoquatro: string = 'undefined'
+console.log(tipoquatro);
+
+let textotres: string = 'Quantas cadeiras tem no laboratório?'
+console.log(textotres);
+
+let quantidadeDeCadeiras; 
+console.log(quantidadeDeCadeiras);
+
+let tipocinco: string = 'objeto';
+console.log(tipocinco);
+
+let pessoa = {
+    nome: 'Isabele',
+    idade: 19,
+};
+console.log(pessoa.nome); // Isabele
+console.log(pessoa.idade); // 19
+
+
